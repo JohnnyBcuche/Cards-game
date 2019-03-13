6ead0883795220476fbe6e557039960d15ca04bc
JohnnyBcuche <kisivan92@gmail.com>
2019-03-13 12:30:27 +0100

    Updates

---
+++ precache-manifest.4a52712eb5fe6fe2f49ffb615be38836.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "88f736d6640ee997ab9c",
+    "url": "/Cards-game/static/js/runtime~main.57e10f11.js"
+  },
+  {
+    "revision": "4774eb113b60cfb22451",
+    "url": "/Cards-game/static/js/main.3d4ed554.chunk.js"
+  },
+  {
+    "revision": "cfbda0e9061432ca6501",
+    "url": "/Cards-game/static/js/2.f213e47f.chunk.js"
+  },
+  {
+    "revision": "4774eb113b60cfb22451",
+    "url": "/Cards-game/static/css/main.d0698d86.chunk.css"
+  },
+  {
+    "revision": "d42406287f97c7c6793dd35829f3159f",
+    "url": "/Cards-game/index.html"
+  }
+];
